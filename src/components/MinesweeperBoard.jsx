776b5871a726e33da83f4df8b84b879a596c3f7e
rk097 <ryddkm@gmail.com>
2025-10-31 21:54:41 -0700

    3x3 gameboard | delete files

--- src/components/MinesweeperBoard.jsx
+++ src/components/MinesweeperBoard.jsx
@@ -1,7 +1,29 @@
 import { useState } from 'react';
+import './minesweeper-board.css'
+
+const Tile = ({ value }) => {
+    return (
+    <button 
+      className="minesweeper-tile">
+        {value}
+    </button>
+  )
+}
 
 const MinesweeperBoard = () => {
-    return <p>hello world</p>;
+    const [Tiles, setTiles] = useState(Array(9).fill(null));
+
+    return (
+        <div className="minesweeper-board-container">
+            <div className="minesweeper-board">
+                {
+                    Tiles.map((value, index) => (
+                        <Tile value={value} />
+                    ))
+                }
+            </div>
+        </div>
+    );
 }
 
 export default MinesweeperBoard;
