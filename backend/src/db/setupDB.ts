1458e2319ff307f096d9713b5c530ee3a55c7b2e
Christina Wang <wyming@s-MacBook-Pro.local>
2025-11-05 19:03:35 -0800

    setup database schema
     Your branch is up to date with 'origin/database'.
    	#setup databse schq
    
    wq
    
    :wq
    :wq
    :::::::

---
+++ backend/src/db/setupDB.ts
@@ -0,0 +1,23 @@
+import { pool } from "./db"
+
+export async function setupDB(){
+    try{
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                password_hash TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT NOW()
+            );
+        `);
+        console.log("tables created successfully");
+    } catch(err){
+        console.error("error creating tables:", err);
+    } finally {
+        pool.end();
+    }
+    
+}
+
+setupDB();
