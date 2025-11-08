077e18c13dcd1cf5bac0b4803f16634994521ed7
ClaraY05 <clara.w.yee@gmail.com>
2025-11-07 18:28:35 -0800

    Populated Sound Page Added checkbox and Slider components from settings/components folder.

--- frontend/src/pages/Settings/Sound.tsx
+++ frontend/src/pages/Settings/Sound.tsx
@@ -1,5 +1,19 @@
+import Slider from "./components/CommonSlider";
+import Checkbox from "./components/CommonCheckbox";
+
 const Sound = () => {
-    return <h2>Sound</h2>
+    return (
+        <div>
+            <h2>Sound</h2>
+                <Slider label="music"/>
+                <Slider label="sfx"/>
+                <Slider label="stereo"/>
+                <Checkbox label="Sound 1"/>
+                <Checkbox label="Sound 2"/>
+                <Checkbox label="Sound 3"/>
+                <Checkbox label="Sound 4"/>
+        </div>
+    );
 }
 
 export default Sound;
