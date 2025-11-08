c30f2d9bc306bc63b6833c4c21f2c8cacb0a92b5
ClaraY05 <clara.w.yee@gmail.com>
2025-11-07 18:44:31 -0800

    Populated notifications settings sub page

--- frontend/src/pages/Settings/Notifications.tsx
+++ frontend/src/pages/Settings/Notifications.tsx
@@ -1,5 +1,14 @@
+import Selector from "./components/CommonSelector";
+
 const Notifications = () => {
-    return <h2>Notifications</h2>
+    let bool: string[] = ["No","Yes"];
+    return (
+        <div>
+            <h2>Notifications</h2>
+            <Selector label="Game Updates" options={bool} defaultValue="Yes"/>
+            <Selector label="Friend Requests" options={bool} defaultValue="Yes"/>
+        </div>
+    );
 }
 
 export default Notifications;
