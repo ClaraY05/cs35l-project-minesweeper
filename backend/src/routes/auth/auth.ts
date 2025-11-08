e6eb1bdf84d49eeb409aa301fa03ab75a70b09d8
Christina Wang <wyming@s-MacBook-Pro.local>
2025-11-07 17:16:23 -0800

    resolve routes merge, created auth folder

--- backend/src/routes/auth/auth.ts
+++ backend/src/routes/auth/auth.ts
@@ -1,14 +1,10 @@
 import { Router, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken"
-import { pool } from "./db/db"
+import { pool } from "../../db/db"
 
 // routes for our api.
 const router = Router();
-
-router.get("/", (req, res) => {
-    res.send("hello world");
-})
 
 console.log("Environment loaded. PORT:", process.env.PORT);
 
