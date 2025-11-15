30f32b658a18277678c43c2e214636fee61d52e4
ClaraY05 <clara.w.yee@gmail.com>
2025-11-14 22:44:16 -0800

    Completed unstyled account display component Added AccountDisplay which displays a users name, gamertag, and profile picture. These are all consistently displayed after signin through the header in the layout page.

---
+++ frontend/src/pages/components/AccountDisplay.tsx
@@ -0,0 +1,17 @@
+interface AccountDisplayProps {        
+    userName: string;
+    gamerTag: string;
+    imgUrl: string;
+}
+
+const AccountDisplay:React.FC<AccountDisplayProps> = ({ userName, gamerTag, imgUrl })=>{
+    return (
+        <div>
+        <strong>{userName}</strong>
+        <em>@{gamerTag}</em>
+        <img src={imgUrl} alt="User Profile" style={{ width: "75px", height: "auto" }}/>
+        </div>
+    );
+};
+
+export default AccountDisplay;
