225be2a182642c154aa1984c6c5fefbbeef74ea1
Christina Wang <wyming@s-MacBook-Pro.local>
2025-11-20 23:16:32 -0800

    clarify usage of username and email, username only needed during register, email and password sufficient for login

--- backend/src/db/setupDB.ts
+++ backend/src/db/setupDB.ts
@@ -6,7 +6,7 @@ export async function setupDB(){
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
                 username VARCHAR(50) UNIQUE NOT NULL,
-                email VARCHAR(50),
+                email VARCHAR(50) UNIQUE NOT NULL,
                 password_hash TEXT NOT NULL,
                 created_at TIMESTAMP DEFAULT NOW()
             );
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -3,8 +3,9 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom"
 
 type Inputs = {
-    username: string;
+    username?: string;
     password: string;
+    email: string
 };
 
 type AuthMode = "login" | "register";
@@ -18,18 +19,18 @@ function Login() {
 
     const navigate = useNavigate();
 
-    // let accessToken: string | null = null;
-
     const onSubmit = async (data: Inputs) => {
         setError("");
         try {
             setLoading(true);
 
             const endpoint = mode==="login" ? "http://localhost:8000/api/auth/login" : "http://localhost:8000/api/auth/register";
+            const dataBody = mode==="login" ? {email:data.email, password:data.password} : {email:data.email, password:data.password, username:data.username}
+            
             const res = await fetch(endpoint, {
                 method: 'POST',
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(data)
+                body: JSON.stringify(dataBody)
             });
 
             const payload = await res.json();
@@ -37,9 +38,7 @@ function Login() {
                 throw new Error(payload.error || "Invalid username or password");
             }
             localStorage.setItem('token',payload.token);
-            if(payload.user){
-                localStorage.setItem('user', JSON.stringify(payload.user));
-            }
+            localStorage.setItem('user', JSON.stringify(payload.user));
             navigate("/", {replace:true});
         } catch (err: any) {
             setError(err.message);
@@ -59,27 +58,43 @@ function Login() {
 
     return (
         <> 
-            <h2>Login Form</h2>
+            <h2>{mode==="login" ? "Login Form" : "Register Form"}</h2>
             <button type="button" onClick={toggle}>
                 {mode==="login" ? "Don't have an account? Register" : "Already have an account? Login"}
             </button>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input id="username" type="email" {...register("username",{
-                    required:"Email is required", pattern:{
-                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                        message: "Invalid email address"
-                    }})}/>
-                {errors.username && <p style={{color: 'red'}}>{errors.username.message}</p>}
-                <input id="password" type="password" {...register("password", {
-                    required: "Password is required", pattern:{
-                        value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                        message: "Password must be at least 8 characters with uppercase, lowercase, number, and special character"
-                    }
-                })
-                }/>
-                {errors.password && <p style={{color: 'red'}}>{errors.password.message}</p>}
-                {error && <p style={{color: 'red'}}>{error}</p>}
-                <button type="submit" disabled={loading}> {loading ? mode==="login"? "Logging in..." : "Signing up...": mode==="login"? "Login" : "Register"}</button>
+                <div>
+                    <input id="email" type="email" {...register("email",{
+                        required:"Email is required", pattern:{
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Invalid email address"
+                        }})}/>
+                    {errors.email && <p style={{color: 'red'}}>{errors.email.message}</p>}
+                </div>
+                {
+                    mode==="register"&&(
+                        <div>
+                            <input id="username" type="text" {...register("username",{
+                                required:"Username is required", minLength:{value:5, message: "Username must be at least 5 characters"}
+                            })}/>
+                            {errors.username && <p style={{color: 'red'}}>{errors.username.message}</p>}
+                        </div>
+                    )
+                }
+                <div>
+                    <input id="password" type="password" {...register("password", {
+                        required: "Password is required", pattern:{
+                            value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                            message: "Password must be at least 8 characters with uppercase, lowercase, number, and special character"
+                        }
+                    })
+                    }/>
+                    {errors.password && <p style={{color: 'red'}}>{errors.password.message}</p>}
+                    {error && <p style={{color: 'red'}}>{error}</p>}
+                </div>
+                <div>
+                    <button type="submit" disabled={loading}> {loading ? mode==="login"? "Logging in..." : "Signing up...": mode==="login"? "Login" : "Register"}</button>
+                </div>
             </form>
         </>
     )
