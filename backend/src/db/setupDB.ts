1650b6c320a9e8848bea0a45c861ff6b7eedd9b8
Christina Wang <wyming@s-MacBook-Pro.local>
2025-11-22 13:33:01 -0800

    edited scores database to include game end status

--- backend/src/db/setupDB.ts
+++ backend/src/db/setupDB.ts
@@ -13,9 +13,12 @@ export async function setupDB(){
             );
         `);
         await pool.query(`
-            CREATE TABLE IF NOT EXTSTS scores (
+            CREATE TABLE IF NOT EXISTS scores (
                 score_id INT GENERATED ALWAYS AS IDENTITY,
-                score INT NOT NULL,
+                score INT,
+                game_id INT NOT NULL,
+                game_status TEXT NOT NULL,
+                diff_level TEXT NOT NULL,
                 created_at TIMESTAMPE DEFAULT NOW(),
                 PRIMARY KEY(score_id),
                 CONSTRAINT fk_user
