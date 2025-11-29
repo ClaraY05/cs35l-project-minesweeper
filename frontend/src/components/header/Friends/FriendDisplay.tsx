8cc2ec87b017031e69f15f5960523719db69a7a1
ClaraY05 <clara.w.yee@gmail.com>
2025-11-29 01:23:04 -0800

    Added styling for friends list popup

--- frontend/src/components/header/Friends/FriendDisplay.tsx
+++ frontend/src/components/header/Friends/FriendDisplay.tsx
@@ -12,17 +12,12 @@ interface FriendProps extends Friend {
 
 const Friends = ({ id, name, avatar, onRemove}:FriendProps) =>{
     return (
-        <div>
-            <div>
-                <strong>{name}</strong>
-                <em>@{id}</em>
+        <div className="flex flex-row gap-2 p-3 bg-stone-900 rounded-sm items-center">
+            <img src={avatar} alt={`Friend ${name}'s Profile`} style={{ width: "100px", height: "100px" }}/>
+            <div className="flex flex-col gap-2 items-start justify-between p-3">
+                <strong className="friend-name">@{name}</strong>
+                <button onClick={() => onRemove(id)} className="uppercase text-xs font-bold hover:text-red-500">Remove Friend</button>
             </div>
-            <img src={avatar} alt={`Friend ${name}'s Profile`} style={{ width: "100px", height: "auto" }}/>
-            <button
-                onClick={() => onRemove(id)}
-            >
-                Remove Friend
-            </button>
         </div>
     );
 };
--- frontend/src/components/header/Friends/TextInput.tsx
+++ frontend/src/components/header/Friends/TextInput.tsx
@@ -14,12 +14,13 @@ const TextInput = ({placeholder = "Enter text...", value="", onChange, onSubmit,
   };
 
   return (
-    <form onSubmit={handleSubmit} >
+    <form onSubmit={handleSubmit} className="w-full flex gap-2">
       <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
+        className="flex-1 bg-stone-900 rounded-sm p-1 focus:outline-none focus:border focus:border-white"
       />
 
       <button
