b30f96f9eec0bd2a73a7ce71ee86e31707ec99f5
rk097 <ryddkm@gmail.com>
2025-11-29 11:29:37 -0800

    code indentation

--- frontend/src/pages/Play/Play.tsx
+++ frontend/src/pages/Play/Play.tsx
@@ -4,9 +4,9 @@ import DifficultySelect, { Difficulty } from "./components/DifficultySelect";
 import './play.css';
 
 const difficultyConfigs: Record<Difficulty, { rows: number; cols: number; mines: number }> = {
-  easy:   { rows: 9,  cols: 9,  mines: 10 },
-  medium: { rows: 16, cols: 16, mines: 40 },
-  hard:   { rows: 16, cols: 30, mines: 99 },
+    easy:   { rows: 9,  cols: 9,  mines: 10 },
+    medium: { rows: 16, cols: 16, mines: 40 },
+    hard:   { rows: 16, cols: 30, mines: 99 },
 };
 
 const Play = () => {
