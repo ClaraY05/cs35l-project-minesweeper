4c91b211c72376fa9de894c16355b6ce7fe74432
ClaraY05 <clara.w.yee@gmail.com>
2025-11-29 01:23:04 -0800

    Added styling for friends list popup

--- frontend/src/components/header/Friends/Friends.tsx
+++ frontend/src/components/header/Friends/Friends.tsx
@@ -1,20 +1,11 @@
 import { useState } from "react";
-import TextInput from "./TextInput"
-import FriendDisplay from "./FriendDisplay"
 import icon from "./person-group-svgrepo-com.svg"
+import FriendsPopout from "./FriendsPopout"
 
 interface UserFriends{
     friendIDList:string[]; // fetch actual friend info from DB holding account info of every player
     dummyRemove: (id: string) => void;
 }
-
-type RemoveCallback = (id: string) => void; // type from Friend Display
-
-const renderFriendDisplay = (friendID:string, dummyRemove:RemoveCallback) => {
-    return(
-        <FriendDisplay id={friendID} name="Friend Name" avatar="https://i.redd.it/help-me-find-the-cat-or-og-picture-from-the-cat-owl-meowl-v0-dghbx7likhgf1.jpg?width=1200&format=pjpg&auto=webp&s=45a83cd201b14934ad2000bf7834a4b92296f4a0" onRemove={dummyRemove}/>
-    );
-};
 
 const Friends = ({ friendIDList = [], dummyRemove}: UserFriends) => {
   const [showOverlay, setShowOverlay] = useState(false);
@@ -33,32 +24,15 @@ const Friends = ({ friendIDList = [], dummyRemove}: UserFriends) => {
         <img src={icon} alt="People Icon flex-shrink-0"/>
       </button>
 
-      {showOverlay && (
-        <div className="Overlay">
-          <h2 className="font-bold">Friends</h2>
-          <div>
-            <h3>Search Friends</h3>
-            <TextInput placeholder="Search gamertag..." value={text} onChange={setText} onSubmit={handleSearch}/>
-          </div>
-
-          <div>
-            <h3>Friend List</h3>
-            {friendIDList.length === 0 ? (
-              <p>No friends found</p>
-            ) : (
-                friendIDList.map(friendID => (
-                renderFriendDisplay(friendID,dummyRemove)
-              ))
-            )}
-          </div>
-          <button
-            onClick={() => setShowOverlay(false)}
-            className="closeOverlay"
-          >
-            Close
-          </button>
-        </div>
-      )}
+      <FriendsPopout
+        isOpen={showOverlay}
+        onClose={() => setShowOverlay(false)}
+        friendIDList={friendIDList}
+        dummyRemove={dummyRemove}
+        text={text}
+        setText={setText}
+        handleSearch={handleSearch}
+      />
     </div>
   );
 };
