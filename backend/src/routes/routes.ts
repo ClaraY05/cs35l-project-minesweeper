8ce089f3f3b1a27aa6b6d7ce9e6501be0affd5bc
mars200814@gmail.com <mars200814@gmail.com>
2025-11-29 02:46:26 -0800

    added keybinds page to router for postman testing

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,6 +3,7 @@ import gameRoutes from "./game/game";
 import authRoutes from "./auth/auth";
 import friendsRoutes from "./friends/friends";
 import leaderboardRoutes from "./leaderboard/leaderboard";
+import keybindRoutes from "./keybinds/keybinds";
 
 // routes for our api.
 const router = Router();
@@ -11,6 +12,7 @@ router.use("/game", gameRoutes);
 router.use("/auth", authRoutes);
 router.use("/friends", friendsRoutes);
 router.use("/leaderboard", leaderboardRoutes);
+router.use("/keybinds", keybindRoutes);
 
 router.use((req, res) => {
     res.status(404).send("page not found");
