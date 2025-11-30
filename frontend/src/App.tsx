4f312495e80a3c699881f7daa139bdcb44a2766e
rk097 <ryddkm@gmail.com>
2025-11-30 12:46:41 -0800

    protect all pages if user is not logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,7 @@ import Leaderboard from "./pages/Leaderboard/Leaderboard"
 
 import { Routes, Route, Navigate } from 'react-router-dom';
 import AuthRedirect from "./components/AuthRedirect";
+import RequireAuth from "./components/RequireAuth";
 
 // because of react-router conventions, App now contains all available routes rather than the homepage.
 // you can add more routes as you see fit.
@@ -29,23 +30,26 @@ const App = () => {
                         <Login />
                     </AuthRedirect>
                 }/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/play" element={<Play/>}/>
-                <Route path="/leaderboard" element={<Leaderboard/>}/>
-                <Route path="/tutorial" element={<Tutorial/>}>
-                    <Route index element={<Navigate to="basics" replace />} /> 
-                    <Route path="basics" element={< Basics/>} />
-                    <Route path="multiplayer" element={<Multiplayer />} />
-                    <Route path="powerups" element={<Powerups />} />
-                    <Route path="debuffs" element={<Debuffs />} />
-                    <Route path="tesselation" element={<Tesselation />} />
-                </Route>
-                <Route path="/settings" element={<Settings/>}>
-                    <Route index element={<Navigate to="keybinds" replace />} /> 
-                    <Route path="keybinds" element={< Keybinds/>} />
-                    <Route path="sound" element={<Sound />} />
-                    <Route path="video_interface" element={<VideoInterface />} />
-                    <Route path="notifications" element={<Notifications />} />
+                
+                <Route element={<RequireAuth />}>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/play" element={<Play/>}/>
+                    <Route path="/leaderboard" element={<Leaderboard/>}/>
+                    <Route path="/tutorial" element={<Tutorial/>}>
+                        <Route index element={<Navigate to="basics" replace />} /> 
+                        <Route path="basics" element={< Basics/>} />
+                        <Route path="multiplayer" element={<Multiplayer />} />
+                        <Route path="powerups" element={<Powerups />} />
+                        <Route path="debuffs" element={<Debuffs />} />
+                        <Route path="tesselation" element={<Tesselation />} />
+                    </Route>
+                    <Route path="/settings" element={<Settings/>}>
+                        <Route index element={<Navigate to="keybinds" replace />} /> 
+                        <Route path="keybinds" element={< Keybinds/>} />
+                        <Route path="sound" element={<Sound />} />
+                        <Route path="video_interface" element={<VideoInterface />} />
+                        <Route path="notifications" element={<Notifications />} />
+                    </Route>
                 </Route>
             </Route>
         </Routes>
