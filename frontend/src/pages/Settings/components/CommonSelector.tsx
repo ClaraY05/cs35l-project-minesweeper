3d6deec69a77b898f23a930d98da4f939f2807ec
ClaraY05 <clara.w.yee@gmail.com>
2025-11-30 01:12:28 -0800

    styled common selector component

--- frontend/src/pages/Settings/components/CommonSelector.tsx
+++ frontend/src/pages/Settings/components/CommonSelector.tsx
@@ -19,18 +19,20 @@ interface SelectorProps {
     useEffect(()=> {setSelected(value)},[value]); // sync child with parent if parent's value changed through other reasons
   
     return (
-      <div>
-        <div>{label}</div>
-        <div>
+      <div className="flex flex-col w-full">
+        <div className="uppercase font-semibold text-xl">{label}</div>
+        <div className="flex w-full">
           {options.map(option => (
             <button
               key={option}
               onClick={() => handleSelect(option)}
               style={{
-                border: selected === option ? "2px solid #007bff" : "1px solid gray",
-                backgroundColor: selected === option ? "#e6f0ff" : "#fff",
+                border: selected === option ? "2px solid white" : "1px solid gray",
+                backgroundColor: selected === option ? "rgba(192, 38, 211, 0.5)" : "rgba(0, 0, 0, 0.5)",
                 fontWeight: selected === option ? "bold" : "normal",
+                color: selected === option ? "rgba(255, 255, 255, 1)" : "rgba(255, 255, 255, 0.5)",
               }}
+              className="py-1 px-2 flex-1"
             >
               {option}
             </button>
