c477d2a287d112bb193baf8757980f45223dc2a1
Christina Wang <wyming@s-MacBook-Pro.local>
2025-11-30 08:10:36 -0800

    save keybinds changes in frontend

--- frontend/src/pages/Settings/Keybinds.tsx
+++ frontend/src/pages/Settings/Keybinds.tsx
@@ -1,18 +1,22 @@
 import KeybindSetter from "./components/AddKey";
+import { useLocalStorage } from "usehooks-ts";
+import { DEFAULT_KEYBINDS } from "./defaultSettings";
 
 const Keybinds = () => {
+    const [keybinds,setKeybinds] = useLocalStorage("keybinds", DEFAULT_KEYBINDS);
+    const update = (newChange:any) => setKeybinds((prev:any)=>({ ...prev, ...newChange}));
     return (
         <div className="w-full">
             <div className="p-2">
-                <KeybindSetter defaultAction="Game Action 1" defaultKey="Keybind 1" thisClassName="Game"/>
-                <KeybindSetter defaultAction="Game Action 2" defaultKey="Keybind 2" thisClassName="Game"/>
-                <KeybindSetter defaultAction="Game Action 3" defaultKey="Keybind 3" thisClassName="Game"/>
-                <KeybindSetter defaultAction="Game Action 4" defaultKey="Keybind 4" thisClassName="Game"/>
+                <KeybindSetter defaultAction="Open Cell" nowKey={keybinds.openCell} thisClassName="Game" onChange={(e)=>update({openCell:e})}/>
+                <KeybindSetter defaultAction="Flag Cell" nowKey={keybinds.flagCell} thisClassName="Game" onChange={(e)=>update({flagCell:e})}/>
+                <KeybindSetter defaultAction="Chord" nowKey={keybinds.chord} thisClassName="Game" onChange={(e)=>update({chord:e})}/>
+                <KeybindSetter defaultAction="Escape Game" nowKey={keybinds.escapeGame} thisClassName="Game" onChange={(e)=>update({escapeGame:e})}/>
             </div>
             <div className="p-3">
-                <KeybindSetter defaultAction="Sub Action 1" defaultKey="Keybind 1" thisClassName="Sub"/>
-                <KeybindSetter defaultAction="Sub Action 2" defaultKey="Keybind 2" thisClassName="Sub"/>
-                <KeybindSetter defaultAction="Sub Action 3" defaultKey="Keybind 3" thisClassName="Sub"/>
+                <KeybindSetter defaultAction="Restart Game" nowKey={keybinds.restartGame} thisClassName="Sub" onChange={(e)=>update({restartGame:e})}/>
+                <KeybindSetter defaultAction="Powerup I" nowKey={keybinds.powerup1} thisClassName="Sub" onChange={(e)=>update({powerup1:e})}/>
+                <KeybindSetter defaultAction="Powerup II" nowKey={keybinds.powerup2} thisClassName="Sub" onChange={(e)=>update({powerup2:e})}/>
             </div>
         </div>
     )
