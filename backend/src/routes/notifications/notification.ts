c1c371ee1d06f6a7fc8645cd800f64e1856bfa78
Jeff <goldendudey@gmail.com>
2025-11-30 16:43:54 -0800

    added routes to create/delete notifications

---
+++ backend/src/routes/notifications/notification.ts
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import { authenticateToken, AuthRequest } from "../middleware/authMiddleware";
+import { getNotifications, createNotification, readNotification, clearAllNotifications } from "./notificationHelpers";
+
+const notificationRoutes = Router();
+
+// get notification for a user
+notificationRoutes.get("/", authenticateToken, async (req: AuthRequest, res) => {
+    try {
+        const userID = req.user?.userID;
+        if (!userID) {
+            return res.status(400).json({ error: "Missing user ID in token" });
+        }
+        const notifications = await getNotifications(userID);
+        return res.json(notifications);
+    }
+    catch (err) {
+        console.error("error in geeting notifications", err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// create a new notification
+notificationRoutes.post("/", authenticateToken, async (req: AuthRequest, res) => {
+    try {
+        const userID = req.user?.userID;
+        const { message, type, comesFromID } = req.body;
+        if (!userID || !message || !type || !comesFromID) {
+            return res.status(400).json({ error: "missing fields"});
+        }
+        const newNotification = await createNotification(userID, message, type, comesFromID);
+        return res.status(201).json(newNotification);
+    }
+    catch (err) {
+        console.error("error in creating notification", err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// delete a notification
+notificationRoutes.delete("/:notificationID", authenticateToken, async (req: AuthRequest, res) => {
+    try {
+        const userID = req.user?.userID;
+        const notificationID = Number(req.params.notificationID);
+        if (!userID || !notificationID) {
+            return res.status(400).json({ error: "missing fields"});
+        }
+        const deletedNotification = await readNotification(notificationID);
+        return res.json(deletedNotification);
+    }
+    catch (err) {
+        console.error("error in deleting notification", err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// clear all notifications
+notificationRoutes.delete("/", authenticateToken, async (req: AuthRequest, res) => {
+    try {
+        const userID = req.user?.userID;
+        if (!userID) {
+            return res.status(400).json({ error: "missing fields"});
+        }
+        const deletedNotifications = await clearAllNotifications(userID);
+        return res.json(deletedNotifications);
+    }
+    catch (err) {
+        console.error("error in clearing all notifications", err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
--- backend/src/routes/notifications/notificationHelpers.ts
+++ backend/src/routes/notifications/notificationHelpers.ts
@@ -1,22 +1,6 @@
 import { pool } from "../../db/db";
 import { addFriend } from "../friends/friendHelpers";
 
-// create a new notification
-export async function createNotification(userID: number, message: string, type: string, comesFromID: number){
-    try {
-        const result = await pool.query(`
-            INSERT INTO notifications (user_id, message, type, comes_from_ID)
-            VALUES ($1, $2, $3, $4)
-            RETURNING notification_id, user_id, message, type, comes_from_ID, is_read, created_at
-        `, [userID, message, type, comesFromID]);
-        return result.rows[0];
-    }
-    catch (err) {
-        console.error("Error creating notification:", err);
-        throw err;
-    }
-}
-
 // get all the notifications for some user
 export async function getNotifications(userID: number){
     try {
@@ -40,6 +24,22 @@ export async function getNotifications(userID: number){
     }
     catch (err) {
         console.error("Error getting notifications:", err);
+        throw err;
+    }
+}
+
+// create a new notification
+export async function createNotification(userID: number, message: string, type: string, comesFromID: number){
+    try {
+        const result = await pool.query(`
+            INSERT INTO notifications (user_id, message, type, comes_from_ID)
+            VALUES ($1, $2, $3, $4)
+            RETURNING notification_id, user_id, message, type, comes_from_ID, is_read, created_at
+        `, [userID, message, type, comesFromID]);
+        return result.rows[0];
+    }
+    catch (err) {
+        console.error("Error creating notification:", err);
         throw err;
     }
 }
