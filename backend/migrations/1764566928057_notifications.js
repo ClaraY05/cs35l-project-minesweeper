e84b83d402a1f88c806fb46048f3644ba94004ca
rk097 <ryddkm@gmail.com>
2025-11-30 21:31:51 -0800

    fix notification migration

--- backend/migrations/1764566928057_notifications.js
+++ backend/migrations/1764566928057_notifications.js
@@ -1,6 +1,14 @@
-// This is the table for notifications
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
 
-exports.up = (pgm) => {
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
     pgm.createTable("notifications", {
         notification_id: { type: "serial", primaryKey: true },
         user_id: {
@@ -22,6 +30,11 @@ exports.up = (pgm) => {
     });
 };
 
-exports.down = (pgm) => {
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
     pgm.dropTable("notifications");
 };
