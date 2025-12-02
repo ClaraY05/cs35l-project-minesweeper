7875d1d5ab199757fcf3dd52c2cc74544f68b371
Christina Wang <yimingwang1005@gmail.com>
2025-12-01 21:56:47 -0800

    removed authentication token generation during register

--- backend/src/routes/auth/auth.ts
+++ backend/src/routes/auth/auth.ts
@@ -54,8 +54,8 @@ authRoutes.post("/register", async(req:Request, res:Response) => {
             const emailToken = await createVerificationToken(userID);
             await sendVerificationEmail(email, emailToken);
 
-            const token = signJwt({ userID, email, username });
-            res.cookie("token",token,{httpOnly:true, secure:false, sameSite:"lax", maxAge:60*60*1000});
+            // const token = signJwt({ userID, email, username });
+            // res.cookie("token",token,{httpOnly:true, secure:false, sameSite:"lax", maxAge:60*60*1000});
 
 
             return res.status(201).json({user:{user_id:userID, email:email, username:username, profile_picture: result.rows[0].profile_picture},
