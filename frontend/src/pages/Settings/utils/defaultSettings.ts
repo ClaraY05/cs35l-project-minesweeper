299c9423da519ad94116e60259e2f2b2583feb5e
Christina Wang <yimingwang1005@gmail.com>
2025-12-03 01:01:18 -0800

    update default settings

--- frontend/src/pages/Settings/utils/defaultSettings.ts
+++ frontend/src/pages/Settings/utils/defaultSettings.ts
@@ -12,15 +12,15 @@ export const DEFAULT_SOUND = {
     music:50,
     sfx:50,
     stereo:50,
-    sound1:false,
-    sound2:false,
-    sound3:false,
-    sound4:false
+    sound1:true,
+    sound2:true,
+    sound3:true,
+    sound4:true
 };
 
 export const DEFAULT_VIDEO = {
     graphics:"Low",
-    displayUsername:false,
+    displayUsername:true,
     changeCustomBg:false,
     customBg:"/bg.png",
     textSize:16
