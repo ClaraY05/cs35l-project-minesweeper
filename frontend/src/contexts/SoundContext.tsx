4e5eb398148a69158b18e1ab51af370398d06c48
ClaraY05 <clara.w.yee@gmail.com>
2025-12-04 11:22:40 -0800

    Added context to make managing sound effects cleaner

---
+++ frontend/src/contexts/SoundContext.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, ReactNode } from "react";
+import { useSoundManager } from "../utils/useSoundManager";
+
+type SoundManagerReturn = ReturnType<typeof useSoundManager>;
+const SoundContext = createContext<SoundManagerReturn | undefined>(undefined);
+interface SoundProviderProps {
+  children: ReactNode;
+}
+
+export const SoundProvider: React.FC<SoundProviderProps> = ({ children }) => {
+  const soundManager = useSoundManager();
+
+  return (
+    <SoundContext.Provider value={soundManager}>
+      {children}
+    </SoundContext.Provider>
+  );
+};
+
+export const useSound = (): SoundManagerReturn => {
+  const context = useContext(SoundContext);
+  
+  if (context === undefined) {
+    throw new Error("useSound must be used within a SoundProvider");
+  }
+  
+  return context;
+};
