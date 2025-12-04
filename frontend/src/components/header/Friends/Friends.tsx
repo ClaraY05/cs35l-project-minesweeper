0c4926b693942b8e44d68c09c867c9a22cd866ca
ClaraY05 <clara.w.yee@gmail.com>
2025-12-04 12:04:02 -0800

    Added sound effects to friends icon

--- frontend/src/components/header/Friends/Friends.tsx
+++ frontend/src/components/header/Friends/Friends.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import icon from "./person-group-svgrepo-com.svg"
 import FriendsPopout from "./components/FriendsPopout"
+import { useSound } from "../../../contexts/SoundContext";
 
 interface Friend {
   user_id: number;
@@ -12,10 +13,23 @@ interface Friend {
 const Friends = () => {
   const [showOverlay, setShowOverlay] = useState(false);
 
+  const {playSoundEffect } = useSound();
+
+  const handleClick = (e: React.MouseEvent) =>{
+    playSoundEffect("/audio/SFX/click.wav", "click");
+  };
+  const handleHover = (e: React.MouseEvent) =>{
+    playSoundEffect("/audio/SFX/select.wav", "select");
+  };
+
   return (
     <div className="flex flex-shrink-0">
       <button
-        onClick={() => setShowOverlay(true)}
+        onClick={(e) => {
+          setShowOverlay(true)
+          handleClick(e);
+        }}
+        onMouseEnter={(e)=> handleHover(e)}
         className="showOverlay rounded-sm bg-main ptpb-2.5 pl-3.5 pr-3.5 hover:border-2 transition-all duration-300 cursor-pointer"
       >
         <img src={icon} alt="People Icon flex-shrink-0"/>
